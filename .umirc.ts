19e142a6a1893c3b14f67db57e85d11fef6a43e3
caijf <369756941@qq.com>
2022-10-30 12:16:58 +0800

    docs: 文档配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,53 @@
 import { defineConfig } from 'dumi';
 
+const isDev = process.env.NODE_ENV === 'development';
+const publicPath = !isDev ? '/create-puzzle/' : '/';
+
 export default defineConfig({
   title: 'create-puzzle',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  history: {
+    type: 'hash',
+  },
+  hash: true,
+  publicPath,
+  favicon: 'https://www.caijinfeng.com/favicon.ico',
+  logo: 'https://www.caijinfeng.com/logo.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
+  nodeModulesTransform: {
+    type: isDev ? 'none' : 'all',
+  },
+  targets: {
+    ie: 11,
+  },
+  polyfill: {
+    imports: ['element-remove', 'core-js'],
+  },
+  headScripts: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-22TYFJH82L',
+    },
+    {
+      content: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-22TYFJH82L');
+    `,
+    },
+  ],
+  styles: [
+    `body .__dumi-default-navbar{
+      display: none;
+    }`,
+  ],
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
 });
