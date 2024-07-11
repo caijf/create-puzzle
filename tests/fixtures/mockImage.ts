195cd199299d83be2115be270badb94cea35ccc3
caijf <369756941@qq.com>
2024-07-11 15:14:16 +0800

    test: update

---
+++ tests/fixtures/mockImage.ts
@@ -0,0 +1,42 @@
+let errorMessage = 'load error';
+export function setImageLoadErrorMessage(str: string) {
+  errorMessage = str;
+}
+
+// 控制图片加载成功 或 失败
+let loadSuccess = true;
+export function setImageLoadStatus(status: boolean) {
+  loadSuccess = status;
+}
+
+class XImage extends Image {
+  [x: string]: any;
+  constructor() {
+    super();
+
+    this.width = 360;
+    this.height = 160;
+    setTimeout(() => {
+      if (loadSuccess) {
+        // @ts-ignore
+        this.onload();
+      } else {
+        // @ts-ignore
+        this.onerror(new Error(errorMessage));
+      }
+    }, 100);
+  }
+}
+
+let temp: typeof globalThis.Image;
+
+export function mockImage() {
+  temp = globalThis.Image;
+  globalThis.Image = XImage;
+}
+
+export function restoreImage() {
+  loadSuccess = true;
+  errorMessage = 'load error';
+  globalThis.Image = temp;
+}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,97 +1,37 @@
 /**
  * @jest-environment jsdom
  */
-import { sleep } from 'ut2';
 import createPuzzle, { Point } from '../src';
+import { createSpyConsoleError } from './fixtures/spyConsole';
+import {
+  ResponseMethod,
+  createSpyAjax,
+  setResponseMethod,
+  setResponseStatus,
+} from './fixtures/spyAjax';
+import { mockImage, restoreImage, setImageLoadStatus } from './fixtures/mockImage';
 
-enum ImageLoadStatus {
-  Success,
-  Error,
-}
-let imageLoadStatus = ImageLoadStatus.Success;
-
-Object.defineProperties(globalThis.Image.prototype, {
-  src: {
-    set() {
-      if (imageLoadStatus === ImageLoadStatus.Error) {
-        setTimeout(() => this.onerror(new Error('load error')));
-      } else {
-        setTimeout(() => this.onload());
-      }
-    },
-  },
-  width: {
-    get() {
-      return 360;
-    },
-  },
-  height: {
-    get() {
-      return 160;
-    },
-  },
-});
-
-// ref: https://stackoverflow.com/questions/28584773/xmlhttprequest-testing-in-jest
-enum ResponseMethod {
-  Load,
-  Error,
-}
-// eslint-disable-next-line prefer-const
-let resMethod = ResponseMethod.Load; // 将要触发的响应方法
-// eslint-disable-next-line prefer-const
-let responseStatus = 200; // 将要触发的响应状态码
 const sendMock = jest.fn();
-const spyAjax = jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => {
-  const methods: Record<string, (...args: any[]) => void> = {};
-
-  async function send() {
-    methods.loadstart?.();
-
-    if (resMethod === ResponseMethod.Error) {
-      methods.error(new Error('ajax error'));
-    } else {
-      await sleep(100);
-
-      const res = {
-        target: {
-          response: new Blob(['hello word']),
-          status: responseStatus,
-        },
-      };
-
-      // @ts-ignore
-      methods.load(res);
-    }
-    methods.loadend?.();
-  }
-
-  return {
-    addEventListener: jest.fn().mockImplementation(function (fnName, fn) {
-      methods[fnName] = fn;
-    }),
-    open: jest.fn(),
-    removeEventListener: jest.fn(),
-    send: sendMock.mockImplementation(send),
-    setRequestHeader: jest.fn(),
-  } as any;
+const spyAjax = createSpyAjax({
+  send: sendMock,
 });
 
-const consoleError = jest.fn();
-const spyConsoleError = jest.spyOn(globalThis.console, 'error').mockImplementation(consoleError);
+const spyConsoleError = createSpyConsoleError();
+mockImage();
 
 describe('createPuzzle', () => {
   beforeEach(() => {
-    imageLoadStatus = ImageLoadStatus.Success;
-    resMethod = ResponseMethod.Load;
-    responseStatus = 200;
+    setImageLoadStatus(true);
+    setResponseMethod(ResponseMethod.Load);
+    setResponseStatus(200);
     sendMock.mockClear();
-    consoleError.mockClear();
+    spyConsoleError.mockClear();
   });
 
   afterAll(() => {
     spyAjax.mockRestore();
     spyConsoleError.mockRestore();
+    restoreImage();
   });
 
   it('load url', async () => {
@@ -201,24 +141,24 @@ describe('createPuzzle', () => {
   });
 
   it('ajax error', async () => {
-    expect(consoleError).toHaveBeenCalledTimes(0);
-    resMethod = ResponseMethod.Error;
+    expect(spyConsoleError).toHaveBeenCalledTimes(0);
+    setResponseMethod(ResponseMethod.Error);
     try {
       await createPuzzle('https://example.com/image', { cacheImage: false });
     } catch (err: any) {
       expect(err.message).toBe('ajax error');
     }
-    expect(consoleError).toHaveBeenCalledTimes(1);
+    expect(spyConsoleError).toHaveBeenCalledTimes(1);
   });
 
   it('image load error', async () => {
-    expect(consoleError).toHaveBeenCalledTimes(0);
-    imageLoadStatus = ImageLoadStatus.Error;
+    expect(spyConsoleError).toHaveBeenCalledTimes(0);
+    setImageLoadStatus(false);
     try {
       await createPuzzle('https://example.com/image', { cacheImage: false });
     } catch (err: any) {
       expect(err.message).toBe('load error');
     }
-    expect(consoleError).toHaveBeenCalledTimes(1);
+    expect(spyConsoleError).toHaveBeenCalledTimes(1);
   });
 });
