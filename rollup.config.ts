9e9f0d48174994b01526779213fb03acc63d280b
caijf <369756941@qq.com>
2024-12-18 15:01:33 +0800

    build: 构建配置调整

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,11 +2,12 @@ import type { RollupOptions } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
-export const outputDir = 'dist';
-export const pkgName = 'createPuzzle';
-export const outputFilePrefix = `${outputDir}/${pkgName}`;
-export const commonConfig = {
+const outputDir = 'dist';
+const pkgName = 'createPuzzle';
+const outputFilePrefix = `${outputDir}/${pkgName}`;
+const commonConfig = {
   input: 'src/index.ts',
   plugins: [
     resolve(),
@@ -17,21 +18,43 @@ export const commonConfig = {
   ],
 };
 
-const config: RollupOptions = {
-  ...commonConfig,
-  external: ['ut2', 'util-helpers'],
-  output: [
-    {
-      format: 'cjs',
-      file: `${outputFilePrefix}.cjs.js`,
-      exports: 'named',
-    },
-    {
-      format: 'es',
-      file: `${outputFilePrefix}.esm.js`,
-      exports: 'named',
-    },
-  ],
-};
+const config: RollupOptions[] = [
+  {
+    ...commonConfig,
+    external: ['ut2', 'util-helpers'],
+    output: [
+      {
+        format: 'cjs',
+        file: `${outputFilePrefix}.cjs.js`,
+        exports: 'named',
+      },
+      {
+        format: 'es',
+        file: `${outputFilePrefix}.esm.js`,
+        exports: 'named',
+      },
+    ],
+  },
+  {
+    ...commonConfig,
+    output: [
+      {
+        format: 'umd',
+        file: `${outputFilePrefix}.js`,
+        exports: 'named',
+        name: pkgName,
+        sourcemap: true,
+      },
+      {
+        format: 'umd',
+        file: `${outputFilePrefix}.min.js`,
+        exports: 'named',
+        name: pkgName,
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+  },
+];
 
 export default config;
