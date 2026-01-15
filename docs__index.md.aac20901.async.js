6b1ee383b847477af5591273389fe7319eb88b50
caijf <caijf@users.noreply.github.com>
2026-01-15 10:51:00 +0000

    deploy: 3f55677e2b1a8c09fdc7c436f2bd4d366d19a4e4

---
+++ docs__index.md.aac20901.async.js
@@ -0,0 +1,71 @@
+"use strict";(self.webpackChunkcreate_puzzle=self.webpackChunkcreate_puzzle||[]).push([[935],{31835:function(l,t,n){n.r(t);var d=n(60699),o=n(10516),c=n(9149),p=n(77569),x=n(67885),s=n(58285),m=n(44302),h=n(62744),a=n(61924),r=n(36060),u=n(75271),_=n(73171),e=n(52676);function i(){return(0,e.jsx)(a.dY,{children:(0,e.jsx)(u.Suspense,{fallback:(0,e.jsx)(r.Z,{}),children:(0,e.jsxs)(e.Fragment,{children:[(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"create-puzzle",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#create-puzzle",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"create-puzzle"]}),(0,e.jsxs)("p",{children:[(0,e.jsx)("a",{href:"https://npmjs.com/package/create-puzzle",children:(0,e.jsx)("img",{src:"https://img.shields.io/npm/v/create-puzzle.svg",alt:"npm"})}),_.texts[0].value,(0,e.jsx)("img",{src:"https://img.shields.io/github/license/caijf/create-puzzle.svg",alt:"GitHub"}),_.texts[1].value,(0,e.jsx)("a",{href:"https://github.com/caijf/create-puzzle",children:(0,e.jsx)("img",{src:"https://img.shields.io/github/stars/caijf/create-puzzle?style=social",alt:"GitHub Star"})})]}),(0,e.jsx)("p",{children:_.texts[2].value}),(0,e.jsxs)("p",{children:[_.texts[3].value,(0,e.jsx)("a",{href:"https://caijf.github.io/rc-slider-captcha/",children:_.texts[4].value}),_.texts[5].value]}),(0,e.jsxs)("p",{children:[_.texts[6].value,(0,e.jsx)("a",{href:"https://www.npmjs.com/package/node-puzzle",children:_.texts[7].value}),_.texts[8].value]}),(0,e.jsxs)("h2",{id:"\u4F7F\u7528",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u4F7F\u7528"]}),(0,e.jsxs)("h3",{id:"es",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#es",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"es"]}),(0,e.jsx)("p",{children:_.texts[9].value}),(0,e.jsx)(s.Z,{lang:"shell",children:_.texts[10].value}),(0,e.jsx)(s.Z,{lang:"shell",children:_.texts[11].value}),(0,e.jsx)(s.Z,{lang:"shell",children:_.texts[12].value}),(0,e.jsx)("p",{children:_.texts[13].value}),(0,e.jsx)(s.Z,{lang:"typescript",children:_.texts[14].value}),(0,e.jsxs)("h3",{id:"\u539F\u751F-js-\u5F00\u53D1\u73AF\u5883",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u539F\u751F-js-\u5F00\u53D1\u73AF\u5883",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u539F\u751F js \u5F00\u53D1\u73AF\u5883"]}),(0,e.jsxs)("p",{children:[_.texts[15].value,(0,e.jsx)("code",{children:_.texts[16].value}),_.texts[17].value,(0,e.jsx)("code",{children:_.texts[18].value}),_.texts[19].value]}),(0,e.jsxs)("p",{children:[_.texts[20].value,(0,e.jsx)("code",{children:_.texts[21].value}),_.texts[22].value,(0,e.jsx)("code",{children:_.texts[23].value}),_.texts[24].value,(0,e.jsx)("code",{children:_.texts[25].value}),_.texts[26].value,(0,e.jsx)("a",{href:"https://unpkg.com/create-puzzle/dist/",children:_.texts[27].value}),_.texts[28].value,(0,e.jsx)("a",{href:"https://unpkg.com/create-puzzle@latest/dist/createPuzzle.min.js",children:_.texts[29].value}),(0,e.jsx)("sup",{children:_.texts[30].value}),_.texts[31].value]}),(0,e.jsxs)("h2",{id:"\u4EE3\u7801\u6F14\u793A",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4EE3\u7801\u6F14\u793A",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u4EE3\u7801\u6F14\u793A"]})]}),(0,e.jsx)(a.Dl,{demo:{id:"docs-demo-dev-puzzle"},previewerProps:{debug:!0,filename:"src/demos/dev-puzzle.tsx"}}),(0,e.jsx)(a.Dl,{demo:{id:"docs-demo-dev-puzzle-random"},previewerProps:{debug:!0,filename:"src/demos/dev-puzzle-random.tsx"}}),(0,e.jsx)(a.Dl,{demo:{id:"docs-demo-dev-image"},previewerProps:{debug:!0,filename:"src/demos/dev-image.tsx"}}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsxs)("h3",{id:"\u57FA\u7840\u7528\u6CD5",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u57FA\u7840\u7528\u6CD5",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u57FA\u7840\u7528\u6CD5"]})}),(0,e.jsx)(a.Dl,{demo:{id:"docs-demo-basic"},previewerProps:{filename:"src/demos/basic.tsx"}}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsxs)("h3",{id:"\u62FC\u56FE\u751F\u6210\u5668",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u62FC\u56FE\u751F\u6210\u5668",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u62FC\u56FE\u751F\u6210\u5668"]})}),(0,e.jsx)(a.rU,{to:"/generator",children:_.texts[32].value}),(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)("br",{}),(0,e.jsxs)("h2",{id:"api",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#api",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"API"]}),(0,e.jsx)(s.Z,{lang:"typescript",children:_.texts[33].value})]})]})})})}t.default=i},73171:function(l,t,n){n.r(t),n.d(t,{texts:function(){return d}});const d=[{value:" ",paraId:0,tocIndex:0},{value:" ",paraId:0,tocIndex:0},{value:"\u5728\u6D4F\u89C8\u5668\u7AEF\u751F\u6210\u6ED1\u5757\u9A8C\u8BC1\u7801\u7684\u62FC\u56FE\u548C\u80CC\u666F\u56FE\u3002",paraId:1,tocIndex:0},{value:"\u53EF\u4EE5\u5728\u5BA2\u6237\u7AEF\u751F\u6210\u62FC\u56FE\uFF0C\u4E5F\u53EF\u4EE5\u4F7F\u7528\u5B83\u751F\u6210\u7684\u62FC\u56FE\u653E\u5230\u670D\u52A1\u7AEF\u56FE\u5E93\uFF0C\u63A8\u8350\u642D\u914D ",paraId:2,tocIndex:0},{value:"rc-slider-captcha",paraId:2,tocIndex:0},{value:" \u4F7F\u7528\u3002",paraId:2,tocIndex:0},{value:"\u5982\u679C\u4F60\u4F7F\u7528\u7684\u662F Node.js \u505A\u670D\u52A1\u7AEF\uFF0C\u63A8\u8350\u4F7F\u7528 ",paraId:3,tocIndex:0},{value:"node-puzzle",paraId:3,tocIndex:0},{value:"\u3002",paraId:3,tocIndex:0},{value:"\u5B89\u88C5\u4F9D\u8D56",paraId:4,tocIndex:2},{value:`npm install create-puzzle
+`,paraId:5,tocIndex:2},{value:`yarn add create-puzzle
+`,paraId:6,tocIndex:2},{value:`pnpm add create-puzzle
+`,paraId:7,tocIndex:2},{value:"\u9879\u76EE\u4E2D\u4F7F\u7528",paraId:8,tocIndex:2},{value:`import { createPuzzle } from 'create-puzzle';
+
+createPuzzle(imgUrl).then((res) => {
+  console.log(res);
+  // {
+  //   bgUrl:  "data:image/jpeg;base64,/9j/4AAQSk...",
+  //   puzzleUrl :  "data:image/png;base64,iVBORw0KGgo...",
+  //   x: 60,
+  //   y: 0
+  // }
+});
+`,paraId:9,tocIndex:2},{value:"\u5982\u679C\u4F60\u7684\u9879\u76EE\u4F7F\u7528\u7684\u662F\u539F\u751F\u65B9\u5F0F\u5F00\u53D1\uFF0C\u53EF\u4EE5\u5728\u6D4F\u89C8\u5668\u4E2D\u4F7F\u7528 ",paraId:10,tocIndex:3},{value:"script",paraId:10,tocIndex:3},{value:" \u6807\u7B7E\u76F4\u63A5\u5F15\u5165\u6587\u4EF6\uFF0C\u5E76\u4F7F\u7528\u5168\u5C40\u53D8\u91CF ",paraId:10,tocIndex:3},{value:"createPuzzle",paraId:10,tocIndex:3},{value:" \u3002",paraId:10,tocIndex:3},{value:"npm \u5305\u7684 ",paraId:11,tocIndex:3},{value:"dist",paraId:11,tocIndex:3},{value:" \u76EE\u5F55\u4E0B\u63D0\u4F9B\u4E86 UMD \u5305 ",paraId:11,tocIndex:3},{value:"createPuzzle.js",paraId:11,tocIndex:3},{value:" \u4EE5\u53CA ",paraId:11,tocIndex:3},{value:"createPuzzle.min.js",paraId:11,tocIndex:3},{value:"\u3002\u4F60\u4E5F\u53EF\u4EE5\u901A\u8FC7 ",paraId:11,tocIndex:3},{value:"UNPKG",paraId:11,tocIndex:3},{value:" \u4E0B\u8F7D\u5230\u672C\u5730\u8FDB\u884C\u4F7F\u7528\u3002\u6216\u8005\u76F4\u63A5\u4F7F\u7528 ",paraId:11,tocIndex:3},{value:"UNPKG \u7EBF\u4E0A\u7248\u672C",paraId:11,tocIndex:3},{value:"\u6CE8\u610F\u7248\u672C",paraId:11,tocIndex:3},{value:"\u3002",paraId:11,tocIndex:3},{value:"\u70B9\u51FB\u8FDB\u5165",paraId:12},{value:`import { createPuzzle, Options, Result, Point, clearCache } from 'create-puzzle';
+
+function createPuzzle(imgUrl: string | Blob, options?: Options): Promise<Result>;
+
+type Options = {
+  // \u62FC\u56FE
+  borderWidth?: number; // \u63CF\u8FB9\u5BBD\u5EA6\u3002\u9ED8\u8BA4 2
+  borderColor?: string; // \u63CF\u8FB9\u989C\u8272\u3002\u9ED8\u8BA4 rgba(255,255,255,0.7)
+  fillColor?: string; // \u586B\u5145\u989C\u8272\u3002\u9ED8\u8BA4 rgba(255,255,255,0.7)
+  points?:
+    | 2
+    | 3
+    | 4
+    | {
+        top: Point;
+        right: Point;
+        bottom: Point;
+        left: Point;
+      }; // \u62FC\u56FE\u70B9\uFF0C\u4E0D\u4F20\u9ED8\u8BA4\u968F\u673A2/3/4
+  width?: number; // \u5BBD\u5EA6\u3002\u9ED8\u8BA4 60
+  height?: number; // \u9AD8\u5EA6\u3002\u9ED8\u8BA4 60
+  x?: number; // x \u8F74\u504F\u79FB\u503C\uFF0C\u5982\u679C\u4E0D\u4F20\u5185\u90E8\u968F\u673A\u751F\u6210\u3002
+  y?: number; // y \u8F74\u504F\u79FB\u503C\uFF0C\u5982\u679C\u4E0D\u4F20\u5185\u90E8\u968F\u673A\u751F\u6210\u3002
+  margin?: number; // \u4E0A\u4E0B\u5DE6\u53F3\u7559\u767D\u3002\u9ED8\u8BA4 2
+
+  // \u80CC\u666F\u56FE
+  bgWidth?: number; // \u80CC\u666F\u56FE\u5BBD\u5EA6\u3002\u9ED8\u8BA4 \u56FE\u7247\u5BBD\u5EA6
+  bgHeight?: number; // \u80CC\u666F\u56FE\u9AD8\u5EA6\u3002\u9ED8\u8BA4 \u56FE\u7247\u9AD8\u5EA6
+  bgOffset?: [number, number] | ((imgWidth: number, imgHeight: number) => [number, number]); // \u80CC\u666F\u56FE\u504F\u79FB\u503C\u3002 \u9ED8\u8BA4 [0,0]
+
+  // \u4E0A\u4F20\u7684\u56FE\u7247
+  imageWidth?: number; // \u81EA\u5B9A\u4E49\u8F93\u5165\u56FE\u7247\u5BBD\u5EA6\u3002
+  imageHeight?: number; // \u81EA\u5B9A\u4E49\u8F93\u5165\u56FE\u7247\u9AD8\u5EA6\u3002
+  cacheImage?: boolean; // \u7F13\u5B58\u6700\u8FD1\u52A0\u8F7D\u6210\u529F\u7684\u56FE\u7247\uFF0C\u6700\u5927\u7F13\u5B58\u6570\u91CF\u4E3A 5 \uFF0C\u53EF\u4F7F\u7528 clearCache \u6E05\u7406\u7F13\u5B58\u3002\u9ED8\u8BA4\u4E3A true \u3002
+  ajaxOptions?: AjaxOptions; // ajax \u8BF7\u6C42\u914D\u7F6E\u9879\uFF0C\u5F53\u4F20\u5165\u7684\u56FE\u7247\u4E3A\u5B57\u7B26\u4E32\u65F6\u624D\u4F1A\u89E6\u53D1\u8BF7\u6C42\u3002\u66F4\u591A\u4FE1\u606F\u53EF\u67E5\u9605\uFF1A https://doly-dev.github.io/util-helpers/global.html#AjaxOptions
+
+  // \u5BFC\u51FA\u56FE\u7247
+  bgImageType?: string; // \u80CC\u666F\u56FE\u5BFC\u51FA\u7C7B\u578B\u3002\u9ED8\u8BA4 image/jpeg
+  quality?: number; // \u5BFC\u51FA\u56FE\u7247\u8D28\u91CF\u3002\u9ED8\u8BA4 0.8 \u3002
+  format?: 'dataURL' | 'blob'; // \u5BFC\u51FA\u56FE\u7247\u683C\u5F0F\u3002\u9ED8\u8BA4 dataURL \u3002
+  autoRevokePreviousBlobUrl?: boolean; // \u81EA\u52A8\u91CA\u653E\u4E4B\u524D\u5BFC\u51FA\u7684 blob url \uFF0C\u4EC5\u5728 format='blob' \u65F6\u751F\u6548\u3002\u9ED8\u8BA4 true \u3002
+};
+
+type Result = {
+  bgUrl: string; // \u80CC\u666F\u56FE
+  puzzleUrl: string; // \u62FC\u56FE
+  x: number; // x \u8F74\u504F\u79FB\u503C\u3002\u5982\u679C\u4F7F\u7528\u8BE5\u503C\u6821\u9A8C\uFF0C\u5EFA\u8BAE\u524D\u540E\u9608\u503C\u589E\u51CF 5 \u7684\u8303\u56F4
+  y: number; // y \u8F74\u504F\u79FB\u503C\uFF0C\u7B49\u9AD8\u62FC\u56FE\u65F6\u503C\u59CB\u7EC8\u4E3A 0
+};
+
+// \u62FC\u56FE\u70B9
+enum Point {
+  None, // \u6CA1\u6709
+  Outer, // \u5916\u90E8
+  Inner // \u5185\u90E8
+}
+`,paraId:13,tocIndex:7}]}}]);
